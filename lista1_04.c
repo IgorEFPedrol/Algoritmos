8e8a3e41c78e57229393541d956be4bc7b929c53
Igor Enrico <igorefp@gmail.com>
2023-04-11 11:35:23 -0400

    Create lista1_04.c

---
+++ lista1_04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+	const float pi = 3.14;
+	int circunferencia, raio;
+	float volume;
+	
+	printf("Qual a circunferencia do planeta? ");
+	scanf("%d", &circunferencia);
+	
+	raio = circunferencia / 2;
+	
+	volume = 4 * pi * raio / 3;
+	
+	printf("\nO volume do planeta e: %.2f", volume);
+	
+	return 0;
+}
