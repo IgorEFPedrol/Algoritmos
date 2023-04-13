645ccaf56a05ac3ee89027dbf42c97bbc7531e35
Igor Enrico <igorefp@gmail.com>
2023-04-13 09:40:55 -0400

    Update lista2_01.c

--- lista2_01.c
+++ lista2_01.c
@@ -3,43 +3,48 @@
 
 int main(void)
 {
-	int salario_base, filhos, porcentagem;
-	float salario_final;
-	char conjuge;
-	float verificar_filhos(int salario_base, int filhos, char conjuge);
-	
+	int salario_base, filhos, conjuge;
+	float salario_final, porcentagem;
+	float verificar_filhos(int salario_base, int filhos, int conjuge);
+
 	printf("Qual o salario base: ");
 	scanf("%d", &salario_base);
-	
+
 	printf("\nQuantos filhos menores de 14 anos? ");
 	scanf("%d", &filhos);
-	
-	do
-	{
-		printf("\nO seu conjuge trabalha? (1 para sim, 0 para nao): ");
-		scanf("%c", &conjuge);	
-	}
-	while (conjuge != 1 && conjuge != 0);
-	
-	verificar_filhos(salario_base, int filhos, char conjuge);
 
-	printf("\nO salario final e: ");
-	
-	verificar_filhos(int salario_base, int filhos, char conjuge)
+    printf("\nO seu conjuge trabalha? (1 para sim, 0 para nao): ");
+    scanf("%d", &conjuge);
+
+    if (conjuge == 1 || conjuge == 0)
+    {
+        float verificar_filhos(int salario_base, int filhos, int conjuge);
+    }
+    else
+    {
+        printf("\nO seu conjuge trabalha? (1 para sim, 0 para nao): ");
+        scanf("%d", &conjuge);
+    }
+
+
+
+
+    //Criando uma função para verificar quantos filhos o usuário possui e qual o salário final,
+    // dependendo do numero de filhos e se o conjuge trabalha
+	float verificar_filhos(int salario_base, int filhos, int conjuge);
 	{
 		if (conjuge == 1)
 		{
-			porcentagem = filhos * 10;
-			salario_final = salario_base + salario_base * 0.porcentagem 
+			porcentagem = filhos * 0.10;
+			salario_final = (float)salario_base + salario_base * (0.0 + porcentagem);
+            printf("\nO salario final e: R$%.2f", salario_final);
 		}
 		else
 		{
-			porcentagem = filhos * 5;
-			salario_final = salario base + salario-base * 0.porcentagem
+			porcentagem = filhos * 0.05;
+			salario_final = salario_base + salario_base * (0.0 + porcentagem);
+			printf("\nO salario final e: R$%.2f", salario_final);
 		}
+	}
 }
-	
-		
-}
-
 
