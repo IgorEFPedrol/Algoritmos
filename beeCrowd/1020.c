aa441a5834f7c83d29c89f6e27c9bc96c8832bff
Igor Enrico <igorefp@gmail.com>
2023-05-09 15:35:10 -0300

    Create 1020.c

---
+++ beeCrowd/1020.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    int dias, ano;
+    
+    scanf("%d", &dias);
+ 
+    printf("%d ano(s)\n", ano);
+    
+    printf("%d mes(es)\n", mes);
+    
+    printf("%d dia(s)\n", dias);
+    return 0;
+}
