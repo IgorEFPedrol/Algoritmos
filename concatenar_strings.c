003f3bef2b99c5ee242853f78ca00a3529601d72
Igor Enrico <igorefp@gmail.com>
2023-05-25 17:02:20 -0300

    Create concatenar_strings.c

---
+++ concatenar_strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    char prim_nome[50] = {"Ola "};
+
+    //A funcao strcat() concatena o segundo parametro na string do primeiro para
+    strcat(prim_nome, "Igor ");
+
+    strcat(prim_nome, "Pedrol");
+
+    printf("%s", prim_nome);
+}
